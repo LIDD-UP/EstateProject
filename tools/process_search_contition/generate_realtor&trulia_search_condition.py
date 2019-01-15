5ae1206b3d3808bc0201ef1d43b8a956d58ae8e0
changhui.li <1807103336@qq.com>
2019-01-15 18:03:55 +0800

    trulia网站分析

---
+++ tools/process_search_contition/generate_realtor&trulia_search_condition.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 _*-  
+""" 
+@author:Administrator
+@file: generate_realtor&trulia_search_condition.py
+@time: 2019/1/15
+"""
+import pandas as pd
+
+
+data = pd.read_csv('county_stateabbrevation.csv')
+print(data.head())
+
